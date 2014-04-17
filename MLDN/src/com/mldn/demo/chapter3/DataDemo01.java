fdf5b15502b25342111a1ef58da167efbaac9631
stephenwang1011 <398733146@qq.com>
2014-04-17 22:51:16 +0800

    创建了基础程序设计的两个程序

---
+++ MLDN/src/com/mldn/demo/chapter3/DataDemo01.java
@@ -0,0 +1,13 @@
+package com.mldn.demo.chapter3;
+/*
+ * 验证若数据过长则有可能出现问题
+ * */
+public class DataDemo01 {
+
+	public static void main(String[] args) {
+
+	//	int num = 9999999999999999999999999999999999999999999;//定义整形变量，错误超出范围
+		
+	}
+
+}
---
+++ MLDN/src/com/mldn/demo/chapter3/DataDemo02.java
@@ -0,0 +1,16 @@
+package com.mldn.demo.chapter3;
+/*
+ * 数据的溢出
+ * 
+ * */
+public class DataDemo02 {
+
+	public static void main(String[] args) {
+		
+		int max = Integer.MAX_VALUE;
+		System.out.println("整形最大值："+max);
+		System.out.println("整形最大值+1："+(max+1));
+		System.out.println("整形最大值+2："+(max+2));
+	}
+
+}
