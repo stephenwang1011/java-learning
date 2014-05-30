cc0fbe90823a8b79faf63243d36a45772d4969e9
stephenwang1011 <398733146@qq.com>
2014-05-30 23:15:16 +0800

    1

---
+++ DailyDemo/src/p0508/CopyOfTestNGDemo.java
@@ -0,0 +1,26 @@
+package p0508;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import p0508.base.BaseParpare;
+
+public class CopyOfTestNGDemo extends BaseParpare{
+  @Test(description="搜索google")
+  public void search() {
+	  WebElement input  = driver.findElement(By.id("kw1"));
+	  input.sendKeys("google");
+	  WebElement searchButton  = driver.findElement(By.id("su1"));
+	  searchButton.click();
+  }
+  
+  
+  @Test(description="最大化窗口")
+  public void maxWindow(){
+	   driver.manage().window().maximize();
+  }
+  
+
+
+
+}
